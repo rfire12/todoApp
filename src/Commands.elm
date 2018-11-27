e425b033d48fcaa2d4a3f950ab1a0cb2836c72bc
Raul <raul@raul.com>
2018-11-27 15:49:55 -0400

    Url fetching and decoding

--- src/Commands.elm
+++ src/Commands.elm
@@ -1,34 +1,63 @@
-module Commands exposing (..)
+module Commands exposing (commentsDecoder, decodeState, fetchTodos, fetchTodosUrl, todoDecoder, todosDecoder)
 
+import Constants exposing (constants)
 import Http
 import Json.Decode as Decode
 import Json.Decode.Pipeline exposing (decode, required)
+import Models exposing (..)
 import Msgs exposing (Msg)
 import RemoteData
-import Constants exposing (constants)
 
-fetchPlayers: Cmd Msg
-fetchTodos = 
+
+fetchTodos : Cmd Msg
+fetchTodos =
     Http.get fetchTodosUrl todosDecoder
-    |> RemoteData.sendRequest
-    |> Cmd.map Msgs.OnfetchTodos
+        |> RemoteData.sendRequest
+        |> Cmd.map Msgs.OnFetchTodos
+
 
 fetchTodosUrl : String
-    constants.todosUrl
+fetchTodosUrl =
+    constants.todosURL
 
-todosDecoder: Decode.Decoder (List Todo)
-todosDecoder=
+
+todosDecoder : Decode.Decoder (List Todo)
+todosDecoder =
     Decode.list todoDecoder
 
-todoDecoder: Decode.Decoder Todo
+
+todoDecoder : Decode.Decoder Todo
 todoDecoder =
     decode Todo
-    |> required "id" Decode.String
-    |> required "name" Decode.String
-    |> required "quantity" Decode.String
-    |> required "comments" (Decode.list commentsDecoder)
+        |> required "id" Decode.string
+        |> required "text" Decode.string
+        |> required "quantity" Decode.int
+        |> required "comments" (Decode.list commentsDecoder)
+        |> required "state" decodeState
 
+
+commentsDecoder : Decode.Decoder Comment
 commentsDecoder =
-    decode Comment = 
-        |> required "id" Decode.String
-        |> required "Comment" Decode.String
+    decode Comment
+        |> required "id" Decode.string
+        |> required "Comment" Decode.string
+
+
+decodeState : Decode.Decoder TodoState
+decodeState =
+    Decode.string
+        |> Decode.andThen
+            (\state ->
+                case state of
+                    "Created" ->
+                        Decode.succeed Created
+
+                    "Deleted" ->
+                        Decode.succeed Deleted
+
+                    "Done" ->
+                        Decode.succeed Done
+
+                    _ ->
+                        Decode.fail "invalid state"
+            )
---
+++ src/Constants.elm
@@ -0,0 +1,6 @@
+module Constants exposing (constants)
+
+
+constants =
+    { todosURL = "http://localhost:4000/todos"
+    }
